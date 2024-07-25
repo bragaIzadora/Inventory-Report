24aea191159cc2c9268f0072c5856fe50db484e6
Izadora Braga <izadora_abraga@outlook.com>
2024-07-25 19:11:41 -0300

    requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,25 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_product_report():
+    product = Product(
+        id="12345",
+        product_name="farinha",
+        company_name="Farinini",
+        manufacturing_date="01-05-2021",
+        expiration_date="02-06-2023",
+        serial_number="TY68 409C JJ43 ASD1 PL2F",
+        storage_instructions="precisa ser armazenado em local protegido da luz"
+    )
+
+    expected_report = (
+        "The product 12345 - farinha "
+        "with serial number TY68 409C JJ43 ASD1 PL2F "
+        "manufactured on 01-05-2021 "
+        "by the company Farinini "
+        "valid until 02-06-2023 "
+        "must be stored according to the following instructions: "
+        "precisa ser armazenado em local protegido da luz."
+    )
+
+    assert str(product) == expected_report
